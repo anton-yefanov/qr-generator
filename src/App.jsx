e5549187223d3e482abb051495279dd5e824611e
Anton Yefanov <antonyefanov@gmail.com>
2022-08-05 13:34:13 +0300

    buttons and transition

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
-import axios from "axios";
 import QRCode from "qrcode";
 
-import React, { useState } from "react";
+import { useState } from "react";
 
 import { Input } from "./components/Input";
 import { QrCode } from "./components/QrCode";
 
 export function App() {
   const [url, setUrl] = useState("");
+
   const [qrCode, setQrCode] = useState("");
+
+  const [qrDisplay, setQrDisplay] = useState(false);
+
+  const [loading, setLoading] = useState(false);
 
   const setImageUrl = (event) => {
     setUrl(event.target.value);
@@ -19,7 +23,7 @@ export function App() {
       url,
       {
         width: 1000,
-        margin: 0,
+        margin: 1.2,
       },
       (err, url) => {
         if (err) return console.error(err);
@@ -28,10 +32,6 @@ export function App() {
       }
     );
   };
-
-  const [qrDisplay, setQrDisplay] = useState(false);
-
-  const [loading, setLoading] = useState(false);
 
   return (
     <>
